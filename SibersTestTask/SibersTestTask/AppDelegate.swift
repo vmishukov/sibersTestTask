a981eb971dd471c576c61a5a4a0fba39c3af3dbb
ketamine <vladg3@mail.ru>
2026-07-17 15:38:12 +0700

    InitialCommit

---
+++ SibersTestTask/SibersTestTask/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  SibersTestTask
+//
+//  Created by Vladislav Mishukov on 17.07.2026.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ SibersTestTask/SibersTestTask/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  SibersTestTask
+//
+//  Created by Vladislav Mishukov on 17.07.2026.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
