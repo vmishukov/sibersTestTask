513f369566179d16e2e6ee63054a0042c6dd890f
ketamine <vladg3@mail.ru>
2026-07-18 18:29:14 +0700

    Feature: added saved files tableview

---
+++ SibersTestTask/SibersTestTask/SavedFiles/SavedFilesViewController.swift
@@ -0,0 +1,75 @@
+//
+//  SavedFilesViewController.swift
+//  SibersTestTask
+//
+//  Created by Vladislav Mishukov on 17.07.2026.
+//
+
+import UIKit
+
+final class SavedFilesViewController: UIViewController {
+    
+    private var savedFilesTableView: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.register(SavedFilesTableViewCell.self,
+                           forCellReuseIdentifier: SavedFilesTableViewCell.identifier)
+        return tableView
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        setupNavigationBar()
+        setupUi()
+        setupConstraints()
+    }
+    
+}
+
+// MARK: - PRIVATE METHODS
+private extension SavedFilesViewController {
+    
+    func setupUi() {
+        savedFilesTableView.delegate = self
+        savedFilesTableView.dataSource = self
+        view.addSubview(savedFilesTableView)
+    }
+    
+    func setupConstraints() {
+        NSLayoutConstraint.activate([
+            savedFilesTableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 8),
+            savedFilesTableView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            savedFilesTableView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            savedFilesTableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -8)
+        ])
+    }
+    
+    func setupNavigationBar() {
+        navigationItem.title = "Saved files"
+    }
+}
+
+// MARK: - UITableViewDelegate
+extension SavedFilesViewController: UITableViewDelegate {
+    
+}
+
+// MARK: - UITableViewDataSource
+extension SavedFilesViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        2
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: SavedFilesTableViewCell.identifier, for: indexPath)
+        guard let fileCell = cell as? SavedFilesTableViewCell else { return cell }
+        return fileCell
+    }
+    
+}
+
+#Preview {
+    SavedFilesViewController()
+}
